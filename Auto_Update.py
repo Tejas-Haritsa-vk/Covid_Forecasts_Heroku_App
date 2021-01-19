de234fa799e0f1acd8e9661241dcd1658893ea46
Tejas Haritsa V K <61158867+Tejas-Haritsa-vk@users.noreply.github.com>
2021-01-19 20:26:20 +0530

    covidforecasts_app

---
+++ Auto_Update.py
@@ -0,0 +1,78 @@
+import time
+from datetime import date
+from datetime import datetime as d
+from main_app_v3 import fit_model, forecast_cases, fit_model_active_cases
+from Update_Database_v4 import update_database, forecasted_database
+
+def auto_update(one_off=False):
+    run = False
+    if one_off:
+        time_now = d.now().time().strftime('%H:%M')
+        date_today = date.today().strftime('%d-%m-%Y')
+        try:
+            update_database()
+            fit_model()
+            fit_model_active_cases()
+            forecasted_database()
+            run_status = "Database Updated"
+            
+            with open("Database_Updates.log", "a+") as log_file:
+                log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+           
+        except Exception as error:
+            run_status = "Failed with Error: {}".format(error)
+            
+            with open("Database_Updates.log", "a+") as log_file:
+                log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+                
+        print("run_status:",run_status)
+    else:
+        run = True    
+
+    while run:
+        time_now = d.now().time().strftime('%H:%M')
+        date_today = date.today().strftime('%d-%m-%Y')
+        if time_now == '04:30':
+            try:
+                update_database()
+                fit_model()
+                fit_model_active_cases()
+                forecasted_database()
+                run_status = "Database Updated"
+                
+                with open("Database_Updates.log", "a+") as log_file:
+                    log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                    log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+               
+            except Exception as error:
+                run_status = "Failed with Error: {}".format(error)
+                
+                with open("Database_Updates.log", "a+") as log_file:
+                    log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                    log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+		
+                time.sleep(360)
+                try:
+                    update_database()
+                    fit_model()
+                    forecasted_database()
+                    run_status = "Database Updated"
+                    
+                    with open("Database_Updates.log", "a+") as log_file:
+                        log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                        log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+                        
+                except Exception as error:
+                    run_status = "Failed with Error: {}".format(error)
+                
+                    with open("Database_Updates.log", "a+") as log_file:
+                        log_file.write("  {} | {}    | {}\n".format(date_today, time_now, run_status))
+                        log_file.write("----------------------------------------------------------------------------------------------------------------\n")
+               
+        else:    
+#             print(date_today, time_now)
+            time.sleep(60)
+#             run = False
+#             break
